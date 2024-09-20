77f5b8e5e3c4f7cde8dbb33459084f00f5336b62
Gabriel-Donizetti <gabsdonizetti@gmail.com>
2024-09-20 15:29:56 -0300

    teste deploy

--- generate_keys.sh
+++ generate_keys.sh
@@ -5,6 +5,9 @@ cd src/main/resources;
 openssl genrsa -out rsa.private 2048;
 openssl rsa -in rsa.private -out rsa.public -pubout -outform PEM;
 
+chmod 755 rsa.private;
+chmod 775 rsa.public;
+
 ls -l;
 
 cd ../../..;
