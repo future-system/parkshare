48854e7508c65cd53995579653b6133200995265
Gabriel-Donizetti <gabsdonizetti@gmail.com>
2024-09-20 15:25:08 -0300

    teste deploy

--- generate_keys.sh
+++ generate_keys.sh
@@ -5,6 +5,8 @@ cd src/main/resources;
 openssl genrsa -out rsa.private 2048;
 openssl rsa -in rsa.private -out rsa.public -pubout -outform PEM;
 
+ls;
+
 cd ../../..;
 
 echo "Keys generated successfully";
